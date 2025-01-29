c0b8f6d7cb3c1a69d26f669089e6592fa0fc21e4
beerhunters <113686882+beerhunters@users.noreply.github.com>
2025-01-29 13:58:24 +0300

    some improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ChatMemberStatus
@@ -11,7 +12,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_TOKEN = os.getenv("API_TOKEN")
-STICKER_ID = os.getenv("STICKER_ID")
+STICKER_IDS = [
+    "CAACAgIAAxkBAAENqQdnmgj2PreJnV9zPnYLeQr9oE_6ywACBmYAAl3N0Uj-7tbvioUa5TYE",
+    "CAACAgIAAxkBAAENp0FnmOucMj3bxD-d8KBzb7sqM1RTlQACu1wAAkI8yEgIHgv8-kFWxzYE",
+]
 # Настройка логгера
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -35,8 +39,10 @@ async def new_member_handler(event: ChatMemberUpdated):
         # Проверяем, что это не бот
         if not new_member.is_bot:
             try:
+                # Выбираем случайный стикер
+                sticker_id = random.choice(STICKER_IDS)
                 # Отправляем стикер
-                await bot.send_sticker(chat_id, sticker=STICKER_ID)
+                await bot.send_sticker(chat_id, sticker=sticker_id)
 
                 # Логирование
                 logger.info(
