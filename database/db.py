3668516ad5561a919f0c13a5227a0c90bad62941
beerhunters <113686882+beerhunters@users.noreply.github.com>
2025-03-14 14:24:58 +0300

    some improve

--- database/db.py
+++ database/db.py
@@ -2,11 +2,11 @@ import functools
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
-from app/config import DATABASE_URL
+# from config import DATABASE_URL
 
-# engine = create_async_engine(url="sqlite+aiosqlite:////app/db.sqlite3", echo=True)
+engine = create_async_engine(url="sqlite+aiosqlite:////app/db.sqlite3", echo=True)
 # engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3", echo=True)
-engine = create_async_engine(url=DATABASE_URL, echo=True)
+# engine = create_async_engine(url=DATABASE_URL, echo=True)
 
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
