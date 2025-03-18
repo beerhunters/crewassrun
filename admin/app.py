383e8d02a14126417a4cc1626fc99719f1f65613
beerhunters <113686882+beerhunters@users.noreply.github.com>
2025-03-18 15:17:01 +0300

    some improve

--- admin/app.py
+++ admin/app.py
@@ -535,4 +535,4 @@ app.register_blueprint(admin_bp)
 if __name__ == "__main__":
     logger.info("Запуск Flask приложения")
     # app.run(debug=True, port=5000)
-    app.run(debug=False, host="0.0.0.0", port=5000)  # Для развертывания в Docker
+    app.run(debug=True, host="0.0.0.0", port=5000)  # Для развертывания в Docker
